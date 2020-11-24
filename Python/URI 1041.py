ec55f15b08eb6fd9825b1521bd22e0d5e57bfdba
JotaEntity <60858790+JotaEntity@users.noreply.github.com>
2020-11-24 14:30:24 -0300

    Create URI 1041.py

---
+++ Python/URI 1041.py
@@ -0,0 +1,27 @@
+x = None
+y = None
+lista = None
+
+def read_line():
+  try:
+    return raw_input()
+  except NameError:
+    return input()
+
+lista = read_line().split(" ")
+x = float((lista[0]))
+y = float((lista[1]))
+if x == 0 and y == 0:
+  print("Origem")
+elif x == 0:
+  print("Eixo Y")
+elif y == 0:
+  print("Eixo X")
+elif y > 0 and x > 0:
+  print("Q1")
+elif y > 0 and x < 0:
+  print("Q2")
+elif y < 0 and x < 0:
+  print("Q3")
+elif y < 0 and x > 0:
+  print("Q4")
