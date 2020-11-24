35d939856ddad493152bd1d78730d11801991b63
JotaEntity <60858790+JotaEntity@users.noreply.github.com>
2020-11-24 14:28:05 -0300

    Create URI 1197.cpp

---
+++ C++/URI 1197.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+//Enunciado desse problema está errado, a velocidade deve ser tratada como constante e a aceleração como nula
+int main (){
+
+
+	int v, t;
+	double a;
+	int s;
+	while(cin >> v >> t){
+
+		s = v*t*2;
+
+		cout << s << endl;
+
+
+	}
+
+
+
+}
