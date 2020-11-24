d52624894408d28c8b25a5be9bd95cfb38e12c24
JotaEntity <60858790+JotaEntity@users.noreply.github.com>
2020-11-24 14:30:44 -0300

    Create URI 1037.py

---
+++ Python/URI 1037.py
@@ -0,0 +1,19 @@
+valor = None
+
+def read_numeric():
+  try:
+    return float(raw_input())
+  except NameError:
+    return float(input())
+
+valor = read_numeric()
+if valor >= 0 and valor <= 25:
+  print("Intervalo [0,25]")
+elif valor > 25 and valor <= 50:
+  print("Intervalo (25,50]")
+elif valor > 50 and valor <= 75:
+  print("Intervalo (50,75]")
+elif valor > 75 and valor <= 100:
+  print("Intervalo (75,100]")
+else:
+  print("Fora de intervalo")
