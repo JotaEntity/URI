b245e6a777349b27d98e812102c3160b46c674f4
JotaEntity <60858790+JotaEntity@users.noreply.github.com>
2020-11-24 14:29:13 -0300

    Create URI 1051.py

---
+++ Python/URI 1051.py
@@ -0,0 +1,17 @@
+salario = None
+
+def read_numeric():
+  try:
+    return float(raw_input())
+  except NameError:
+    return float(input())
+
+salario = read_numeric()
+if salario >= 0 and salario <= 2000:
+  print("Isento")
+elif salario > 2000 and salario <= 3000:
+  print(str("R$ ") + str("{:0.2f}".format(((salario - 2000) * 0.08))))
+elif salario > 3000 and salario <= 4500:
+  print(str("R$ ") + str("{:0.2f}".format(((salario - 3000) * 0.18 + 80))))
+else:
+  print(str("R$ ") + str("{:0.2f}".format(((salario - 4500) * 0.28 + 350))))
