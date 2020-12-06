7063e90a61c2e2bc186469b114b85b85fb78eba9
JotaEntity <60858790+JotaEntity@users.noreply.github.com>
2020-12-05 23:10:51 -0300

    Update Simulado Unesp.cpp

--- C++/Strings/Simulado Unesp.cpp
+++ C++/Strings/Simulado Unesp.cpp
@@ -1,1 +1,50 @@
+#include <iostream>
+#include <stack>
 
+using namespace std;
+using std::string;
+
+int main(){
+    
+    int n, i;
+    std::string linha;
+    stack<char> lista;
+    
+    scanf("%i", &n);
+    
+    while(n--){
+    	
+    	  getline(cin, linha);
+    	
+    	if (lista.size() > 0){
+    		lista.pop();
+		}
+		for (i=0; i < linha.size(); i++){
+			if (linha[i] == '[' || linha[i] == '('){
+				lista.push(linha[i]);
+					
+				} else if (lista.empty()){
+					lista.push('E');
+					break;
+				} else if(linha[i] == ')' || lista.top() == '('){
+					lista.pop();
+				} else if(linha[i] == ']' || lista.top() == '['){
+					lista.pop();
+				} else {
+					lista.push('E');
+					break;
+				}
+			}
+			
+			
+			if (lista.size() == 0){
+				cout << "Yes" << endl;
+			} else {
+				cout << "No" << endl;
+			}
+			
+		}
+		
+		
+    return 0;	
+	}
